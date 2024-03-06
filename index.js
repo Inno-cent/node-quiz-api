159726858676feddc43d24243f797e38d7f700dd
innocent <ekweozorinnocent@gmail.com>
2024-03-06 03:07:54 +0100

    server file update

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-require('dotenv').config()
+const routes = require("./routes"); // includes the routes.js file
+const cors = require("cors"); // includes cors module
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
@@ -11,6 +17,7 @@ const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("database connected"));
 
-app.listen(process.env.PORT || 2020, () => {
-  console.log("The API is running...");
+const port = process.env.PORT || 2020
+app.listen(port, () => {
+  console.log(`The API is running onn ${port}...`);
 });
