eb8eb114ac866aba1cc7cbb54b966af762fa8909
innocent <ekweozorinnocent@gmail.com>
2024-03-06 02:47:03 +0100

    server setup

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
-const express = require('express')
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
 
-const app = express()
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("database connected"));
+
+app.listen(process.env.PORT || 2020, () => {
+  console.log("The API is running...");
+});
