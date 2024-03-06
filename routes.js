3921d3fd8f87b19b1abc2087100fb6557336a049
innocent <ekweozorinnocent@gmail.com>
2024-03-06 03:57:21 +0100

    added question by id

--- routes.js
+++ routes.js
@@ -1,12 +1,32 @@
 const express = require("express");
 const router = express.Router();
-const Question = require("./models/Question");
+const Question = require("./models/question");
 
 // get all quiz questions
-router.get("/questions", (req, res) => {});
+router.get("/questions", async (req, res) => {
+  try {
+    const questions = await Question.find();
+    return res.status(200).json(questions);
+  } catch (error) {
+    return res.status(500).json({ error: error });
+  }
+});
 
 // get one quiz question
-router.get("/questions/:id", (req, res) => {});
+router.get("/questions/:id", async (req, res) => {
+  try {
+    const _id = req.params.id;
+
+    const question = await Question.findOne({ _id });
+    if (!question) {
+      return res.status(404).json({});
+    } else {
+      return res.status(200).json(question);
+    }
+  } catch (error) {
+    return res.status(500).json({ error: error });
+  }
+});
 
 // create one quiz question
 router.post("/questions", async (req, res) => {
